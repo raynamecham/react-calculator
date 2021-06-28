dc5bd606fa45c5433bff2dcfaa2a60da738fe3fd
raynamecham <rayna.mecham@gmail.com>
2021-06-28 13:47:36 -0600

    working on components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './Button.css';
 
-const Button = props => (
-    <div>
+const isOperator = val => {
+    return !isNaN(val) || val === "." || val === "=";
+}
+
+export const Button = props => (
+    <div className={`button-wrapper ${
+        isOperator(props.children) ? null : "operator"
+    }`}>
         {props.children}
     </div>
 )
 
 export default Button;
+
